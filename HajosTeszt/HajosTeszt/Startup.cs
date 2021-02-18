40b1172e966c4eb8d8dcb88235f3a3b3eb52a3a0
Szuák Ákos <Akos.Szuak@bitclub.hu>
2021-02-18 14:24:19 +0100

    Második gyakorlat

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -14,11 +14,6 @@ namespace HajosTeszt
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,19 +22,25 @@ namespace HajosTeszt
             }
 
             app.UseHttpsRedirection();
-
-            app.UseDefaultFiles();
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("cv.html");
+            app.UseDefaultFiles(options);
             app.UseStaticFiles();
+
+
+            //app.UseDefaultFiles();
+            //app.UseStaticFiles(); 
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    //await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
         }
     }
 }
